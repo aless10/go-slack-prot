bef02ba83703d84231de4f0ede62c90b407bfcdd
aless10 <alessio.izzo86@gmail.com>
2019-02-28 00:28:04 +0100

    [add] add slack command parsing

--- server.go
+++ server.go
@@ -1,62 +1,58 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/nlopes/slack/slackevents"
 )
 
+type User struct {
+	Login string `json:"login"`
+	Email string
+}
+
+type Label struct {
+	LabelName string `json:"name"`
+}
+
 type Message struct {
-	channel_id   string
-	channel_name string
-	command      string
-	response_url string
-	team_domain  string
-	team_id      string
-	text         string
-	token        string
-	trigger_id   string
-	user_id      string
-	user_name    string
+	//ChannelID   string `json:"channel_id"`
+	//ChannelName string `json:"channel_name"`
+	//Command     string `json:"command"`
+	Text string `json:"text"`
+	//UserID      string `json:"user_id"`
+	UserName string `json:"user_name"`
 }
 
 func messageHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	message := Message{}
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&message)
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	err := r.ParseForm()
 
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(r.Body)
-	body := buf.String()
-	eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: Config.SlackToken}))
-	if e != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	fmt.Println("Events Api: ", eventsAPIEvent)
-	fmt.Println("Request body is \n", body)
-	fmt.Println("Request body message is \n", message)
-	body_json, err := json.Marshal(message)
+	fmt.Printf("%v:%v\n", "Userid", r.Form.Get("user_id"))
+	user_id := r.Form.Get("user_id")
+	response := InChannelResponse{user_id, "in_channel"}
+	js, err := json.Marshal(response)
 	if err != nil {
-		fmt.Printf("%s\n", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Println("Json marshalled: ", body_json)
-	user, err := api.GetUserByEmail("message.text")
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		return
-	}
-	fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+	// user, err := api.GetUseName(message.)
+	// if err != nil {
+	// 	fmt.Printf("%s\n", err)
+	// 	return
+	// }
+	// fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
 }
 
 type InChannelResponse struct {
@@ -88,11 +84,23 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
-type GithubResponse struct {
+type PullRequest struct {
 	HTMLUrl string `json:"html_url"`
 	Number  int    `json:"number"`
 	State   string `json:"state"`
-	Body    string `json:"body"`
+	ByUser  User   `json:"user"`
+}
+
+type GithubResponse struct {
+	Action             string      `json:"action"`
+	PR                 PullRequest `json:"pull_request"`
+	Body               string      `json:"body"`
+	Labels             []Label     `json:"labels"`
+	CreatedAt          string      `json:"created_at"`
+	UpdatedAt          string      `json:"updated_at"`
+	Assignee           User        `json:"assignee"`
+	Assignees          []User      `json:"assignees"`
+	RequestedReviewers []User      `json:"requested_reviewers"`
 }
 
 func githubPrHandler(w http.ResponseWriter, r *http.Request) {
@@ -100,19 +108,31 @@ func githubPrHandler(w http.ResponseWriter, r *http.Request) {
 	var t GithubResponse
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err)
 	}
-	err_2 := json.Unmarshal([]byte(body), &t)
+	jsonErr := json.Unmarshal([]byte(body), &t)
 
-	if err_2 != nil {
-		panic(err)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
 	}
 	log.Println(t)
+	for _, reviewer := range t.RequestedReviewers {
+		log.Println(reviewer)
+	}
+	// To delete when reviewers are available
+	assignee := t.Assignee
+	log.Println(assignee)
+	getUserByEmail(assignee.Email)
 
-	/* buf := new(bytes.Buffer)
-	buf.ReadFrom(r.Body)
-	body := buf.String()
-	fmt.Println("PR body: ", body) */
+}
+
+func getUserByEmail(email string) {
+	user, err := api.GetUserByEmail(email)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	fmt.Printf("ID: %s, Fullname: %s, Email: %s\n", user.ID, user.Profile.RealName, user.Profile.Email)
 }
 
 func registerEndpoints() {
